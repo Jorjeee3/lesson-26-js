ceb29a8a074fbecc5d003eea3f43e7830d00f86c
Jorjeee3 <look7at7george@gmail.com>
2021-03-03 18:07:22 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+
+
+//--------------------TASK-1---------------------
+
+let pruductList = [
+    {
+        name: "Bread",
+        amount: 2,
+        isBought: false,
+    },
+    {
+        name: "Egg",
+        amount: 15,
+        isBought: true,
+    },
+    {
+        name: "Mashroom",
+        amount: 6,
+        isBought: false,
+    },
+];
+
+function renderProducts() {
+    let sortedPruductList = pruductList.sort((a, b) => {
+        return a.isBought - b.isBought
+    });
+    console.log(sortedPruductList)
+}
+
+function addProduct(name) { 
+    console.log('add product')
+    let productAlreadyExists = false;
+    for (let product of pruductList) {
+        if (product.name === name) {
+            product.amount ++;
+            productAlreadyExists = true;
+        } 
+    } 
+
+    if (productAlreadyExists === false) {
+        pruductList.push({
+            name, amount: 1, isBought: false,
+        });
+    }
+}   
+
+renderProducts()
+
+addProduct("Water")
+
+addProduct("Mashroom")
+
+renderProducts()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------TASK-2---------------------
+
+
+
+//--------------------TASK-3---------------------
+
+
+
+
+//--------------------TASK-4---------------------
+
+
+
+//--------------------TASK-5---------------------
+
+
+
+
+//--------------------TASK-6---------------------
+
+//--------------------TASK-7---------------------   
